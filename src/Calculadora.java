0a481376b3035e11338e9654c8df362e4a55cfdb
JuanC <jdiegocifuentes@ucundinamarca.edu.co>
2025-03-02 19:50:46 -0500

    agregue el codigo de calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void  main(String[] args) {
+    int max = 100;
+    int min = 1;
+    int num1;
+    int num2;
+    String signoA;
+
+
+    Scanner scanner = new Scanner(System.in);
+    Random numAle = new Random();
+
+    num1 = numAle.nextInt(max - min + 1) + min;
+    num2 = numAle.nextInt(max - min + 1) + min;
+
+        System.out.println("el primer numero sera " + num1);
+        System.out.println("el segundo numero sera " + num2);
+
+        System.out.println("ingrese la operacion que desee (signo)");
+        signoA = scanner.next();
+
+        double resultado = switch (signoA) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num2!=0 ? (double) num1/ num2: Double.NaN;
+            case "^" -> Math.pow(num1,num2);
+            case "%" -> num2 != 0 ? num1 % num2 : Double.NaN;
+            default -> {
+                System.out.println("el operador ingresado no es valido");
+                yield Double.NaN;
+            }
+        };
+
+        System.out.println("el resultado de "+ num1 +" "+ signoA+" " + num2 + " es: " + resultado);
+
+    }
+}
